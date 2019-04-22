1403db891afdd55cd620d8f42f716349dc5ed073
Taylor Siebenberg <taylor.siebenberg@gmail.com>
2019-04-22 00:17:40 -0600

    Adding date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule }     from './app-routing.module';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+
+import { MatDatepickerModule }  from '@angular/material/datepicker';
+import { MatFormFieldModule }   from '@angular/material';
+import { MatNativeDateModule }  from '@angular/material';
+import {MatInputModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
